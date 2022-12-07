5ca3234a68b0bff119c4569e76bcc2cef163ee00
yousef alslami <yosef0sa@gmail.com>
2022-12-07 08:08:37 +0300

    Create Logs File

--- StudentProgramCsharp/Program.cs
+++ StudentProgramCsharp/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Serilog;
 
 namespace StudentProgramCsharp
 {
@@ -16,21 +17,53 @@ namespace StudentProgramCsharp
         static Form MainForm;
         [STAThread]
         static void Main()
-        { 
+        {
+            //Creating logs file 
+            Log.Logger = new LoggerConfiguration()
+
+                //WriteTo Console
+                .WriteTo.Console()
+
+                //WriteTo File when Log Event Level => Information 
+                .WriteTo.File("logs/log.txt",
+                restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information,
+                rollingInterval: RollingInterval.Day)
+
+                //WriteTo File when Log Event Level => Warning
+                .WriteTo.File("logs/errorlog.txt", restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Warning)
+
+                //CreateLogger
+                .CreateLogger();
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            //Show Splash Form
-            SplashScreen = new SplashScreen();
-            var splashThread = new Thread(new ThreadStart(
-                () => Application.Run(SplashScreen)));
-            splashThread.SetApartmentState(ApartmentState.STA);
-            splashThread.Start();
+            try
+            {
+                Log.Information("Starting our service..");
 
-            //Create and Show Main Form
-            MainForm = new Form1();
-            MainForm.Load += MainForm_LoadCompleted;
-            Application.Run(MainForm);
+                //Show Splash Form
+                SplashScreen = new SplashScreen();
+                var splashThread = new Thread(new ThreadStart(
+                    () => Application.Run(SplashScreen)));
+                splashThread.SetApartmentState(ApartmentState.STA);
+                splashThread.Start();
+
+                //Create and Show Main Form
+                MainForm = new Form1();
+                MainForm.Load += MainForm_LoadCompleted;
+                Application.Run(MainForm);
+            }
+            catch (SystemException ex)
+            {
+                Log.Fatal(ex, "Exception in application");
+            }
+            finally
+            {
+                Log.Information("Exiting service");
+                Log.CloseAndFlush();
+            }
 
         }
 
